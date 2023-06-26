dd6cd7a92ca2064fa06f8f72e21168b4647cad0e
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-06-26 21:51:02 +0530

    retweet a tweet with a click

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ tweetBtn.addEventListener("click", function () {
 document.addEventListener("click", function (e) {
   if (e.target.dataset.like) {
     handleLikeClick(e.target.dataset.like);
+  } else if (e.target.dataset.retweet) {
+    handleRetweetClick(e.target.dataset.retweet);
   }
 });
 
@@ -28,6 +30,22 @@ function handleLikeClick(tweetId) {
   }
 
   targetTweetObj.isLiked = !targetTweetObj.isLiked;
+
+  render();
+}
+
+function handleRetweetClick(tweetId) {
+  const targetTweetObj = tweetsData.filter(function (tweet) {
+    return tweetId === tweet.uuid;
+  })[0];
+
+  if (targetTweetObj.isRetweeted) {
+    targetTweetObj.retweets--;
+  } else {
+    targetTweetObj.retweets++;
+  }
+
+  targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted;
 
   render();
 }
